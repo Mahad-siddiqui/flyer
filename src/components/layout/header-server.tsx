d97561891cc0e11968abb0d40e0d91c59da70e84
Muhammad Mahad Siddiqui <m.mahadsiddiquii@gmail.com>
2025-07-25 14:34:58 +0500

    delete old

--- src/components/layout/header-server.tsx
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/auth'
-import { HeaderClient } from './header-client'
-
-export async function Header() {
-    const session = await getServerSession(authOptions)
-
-    return <HeaderClient session={session} />
-}
