8ab11aa42646f1c3acb95cb8f9c135e8f8029e59
Muhammad Mahad Siddiqui <m.mahadsiddiquii@gmail.com>
2025-08-01 22:09:33 +0500

    Removed .next folder

--- .next/static/chunks/src_app_auth_register_page_tsx_485809b2._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_b490e2d8._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_try-now_page_tsx_41b513df._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_10184982._.js",
-  "static/chunks/src_0e7cae5e._.js"
-],
-    source: "dynamic"
-});
